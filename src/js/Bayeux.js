1b5ba2ebb9f8c11f24f5e9082c94a8d8fc9164b7
Kyle Alexis Sargeant <kasargeant@gmail.com>
2017-06-10 06:35:16 +0200

    Adds ability to select output destination and format.  Moves 'pretty-print' reporting into a separate runner.

--- src/js/Bayeux.js
+++ src/js/Bayeux.js
@@ -7,8 +7,10 @@
  */
 
 // Imports
+const fs = require("fs");
 const assert = require("assert");
 const series = require("async-series");
+const tinter = require("tinter");
 
 class Bayeux {
     constructor() {
@@ -17,52 +19,7 @@ class Bayeux {
         this.fnArray = [];
     }
 
-    _displayJSON(unitReport) {
 
-        let reports = unitReport.tests;
-
-        let testsTotal = 0;
-        let testsFailed = 0;
-        let testsPassed = 0;
-
-        for(let testIdx = 0; testIdx < reports.length; testIdx++) {
-            // Extract each report.
-            let report = reports[testIdx];
-            testsTotal += report.cases.length;
-
-            console.log("");
-            console.log(`  Test Report: ${report.name} (${report.cases.length} tests).`);
-            for(let i = 0; i < report.cases.length; i++) {
-                let result = report.cases[i];
-
-                let name = result.message;
-                let filename = result.file;
-                let line = result.line;
-                let column = result.column;
-
-                if(result.ok === true) {
-                    // e.g. {"id":0,"ok":true,"name":"it should have assigned the right height.","operator":"equal","objectPrintDepth":5,"actual":210,"expected":210,"test":0,"type":"assert"}
-                    testsPassed++;
-                    console.log(`    \x1b[32m✓\x1b[0m ${name}`);
-                } else {
-                    // e.g. {"id":2,"ok":false,"name":"it should have assigned the right area.","operator":"equal","objectPrintDepth":5,"actual":44100,"expected":441100,"error":{},"functionName":"Test.<anonymous>","file":"/Users/kasargeant/dev/projects/warhorse/test/data/client_test/js/tape.js:17:8","line":17,"column":"8","at":"Test.<anonymous> (/Users/kasargeant/dev/projects/warhorse/test/data/client_test/js/tape.js:17:8)","test":0,"type":"assert"}
-                    testsFailed++;
-                    console.log(`    \x1b[31m✕\x1b[0m FAILED: ${name}`);
-                    console.log(`        Testing: '${result.operator}'`);
-                    console.log(`        at line: ${line} col: ${column} in '${filename}'.`);
-                    console.log(`        - expected: '${result.expected}'`);
-                    console.log(`        - actual  : '${result.actual}'`);
-                    console.log("");
-                }
-            }
-        }
-        console.log(`Test Report Summary:`);
-        console.log(`   \x1b[32mTests passed: (${testsPassed}/${testsTotal})\x1b[0m`);
-        if(testsFailed) {
-            console.log(`   \x1b[31mTests failed: (${testsFailed}/${testsTotal})\x1b[0m`);
-        }
-        console.log("");
-    }
 
     report() {
         // Clean-up sequential report into a useful object.
@@ -94,15 +51,29 @@ class Bayeux {
         }
 
 
-        //console.log(JSON.stringify(this.reports, null, 2));
-        console.log(JSON.stringify(unitReport, null, 2));
-        this._displayJSON(unitReport);
+        // Select test output format and destination
+        let format = "json";
+        let destination = "stdout";
+
+        let unitReportSerialized = "";
+        if(format === "json") {
+            unitReportSerialized = JSON.stringify(unitReport, null, 2);
+        }
+
+        if(destination === "stdout") {
+            console.log(unitReportSerialized);
+        }
+        else if(destination === "file") {
+            let filename = unitReport.name.replace(/ /g, "_") + ".out.json";
+            fs.writeFileSync(filename, unitReportSerialized);
+        }
+
         return this.reports;
     }
 
     _report(desc, fn) {
         try {
-            console.log(desc + ": starting...");
+            //console.log(desc + ": starting...");
             fn();
             this.reports.push({
                 type: "case",
@@ -156,7 +127,7 @@ class Bayeux {
 
         series(this.fnArray, function() {
             this.fnArray = [];
-            console.log("SUPERDONE!");
+            console.log("Done!");
             this.report(); // Indicate the test has finished
         }.bind(this));
 
--- src/js/BayeuxRunner.js
+++ src/js/BayeuxRunner.js
@@ -14,6 +14,53 @@ class Bayeux {
 
     }
 
+    _reportJSON(unitReport) {
+
+        let reports = unitReport.tests;
+
+        let testsTotal = 0;
+        let testsFailed = 0;
+        let testsPassed = 0;
+
+        for(let testIdx = 0; testIdx < reports.length; testIdx++) {
+            // Extract each report.
+            let report = reports[testIdx];
+            testsTotal += report.cases.length;
+
+            console.log("");
+            console.log(`  Test Report: ${report.name} (${report.cases.length} tests).`);
+            for(let i = 0; i < report.cases.length; i++) {
+                let result = report.cases[i];
+
+                let name = result.message;
+                let filename = result.file;
+                let line = result.line;
+                let column = result.column;
+
+                if(result.ok === true) {
+                    // e.g. {"id":0,"ok":true,"name":"it should have assigned the right height.","operator":"equal","objectPrintDepth":5,"actual":210,"expected":210,"test":0,"type":"assert"}
+                    testsPassed++;
+                    console.log(`    ${tinter.green("✓")} ${name}`);
+                } else {
+                    // e.g. {"id":2,"ok":false,"name":"it should have assigned the right area.","operator":"equal","objectPrintDepth":5,"actual":44100,"expected":441100,"error":{},"functionName":"Test.<anonymous>","file":"/Users/kasargeant/dev/projects/warhorse/test/data/client_test/js/tape.js:17:8","line":17,"column":"8","at":"Test.<anonymous> (/Users/kasargeant/dev/projects/warhorse/test/data/client_test/js/tape.js:17:8)","test":0,"type":"assert"}
+                    testsFailed++;
+                    console.log(`    ${tinter.red("✕")} FAILED: ${name}`);
+                    console.log(`        Testing: '${result.operator}'`);
+                    console.log(`        at line: ${line} col: ${column} in '${filename}'.`);
+                    console.log(`        - expected: '${result.expected}'`);
+                    console.log(`        - actual  : '${result.actual}'`);
+                    console.log("");
+                }
+            }
+        }
+        console.log(`Test Report Summary:`);
+        console.log(`   \x1b[32mTests passed: (${testsPassed}/${testsTotal})\x1b[0m`);
+        if(testsFailed) {
+            console.log(`   \x1b[31mTests failed: (${testsFailed}/${testsTotal})\x1b[0m`);
+        }
+        console.log("");
+    }
+
     _reportTape(reports) {
         let testsTotal = 0;
         let testsFailed = 0;

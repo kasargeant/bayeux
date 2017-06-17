76ceb089d2e07a290293c1c92e2b8cb85ef59c25
Kyle Alexis Sargeant <kasargeant@gmail.com>
2017-06-17 18:58:47 +0200

    Finishes third possible vocabulary example.

--- test/js/samples/Tinter16.third.js
+++ test/js/samples/Tinter16.third.js
@@ -22,7 +22,7 @@ const DUMMY_STRING = "Dummy String";
 // Unit it(s)
 unit("Class: Tinter (Node/16-color [using CSS Named colors])", function() {
 
-    test("it should correctly encode all style directives.", function(done) {
+    does("it correctly encode all style directives?", function(complete) {
 
         when("it marks a string as reset.").expect(Tinter.reset(DUMMY_STRING)).toEqual(`\x1b[0m${DUMMY_STRING}\x1b[0m`);
         when("it marks a string as plain.").expect(Tinter.plain(DUMMY_STRING)).toEqual(`\x1b[0m${DUMMY_STRING}\x1b[0m`);
@@ -36,10 +36,10 @@ unit("Class: Tinter (Node/16-color [using CSS Named colors])", function() {
         when("it marks a string as inverse.").expect(Tinter.inverse(DUMMY_STRING)).toEqual(`\x1b[7m${DUMMY_STRING}\x1b[0m`);
         when("it marks a string as hidden.").expect(Tinter.hidden(DUMMY_STRING)).toEqual(`\x1b[8m${DUMMY_STRING}\x1b[0m`);
 
-        done(); // Indicate the test has finished
+        complete(); // Indicate the test is complete.
     });
 
-    it("Colorization functions (foreground)", function(done) {
+    does("it correctly encode all foreground color directives?", function(complete) {
 
         when("it marks a string as black.").expect(Tinter.black(DUMMY_STRING)).toEqual(`\x1b[1m\x1b[30m${DUMMY_STRING}\x1b[0m`);
         when("it marks a string as red.").expect(Tinter.red(DUMMY_STRING)).toEqual(`\x1b[1m\x1b[91m${DUMMY_STRING}\x1b[0m`);
@@ -51,10 +51,10 @@ unit("Class: Tinter (Node/16-color [using CSS Named colors])", function() {
         when("it marks a string as white.").expect(Tinter.white(DUMMY_STRING)).toEqual(`\x1b[1m\x1b[97m${DUMMY_STRING}\x1b[0m`);
         when("it marks a string as default.").expect(Tinter.default(DUMMY_STRING)).toEqual(`\x1b[39m${DUMMY_STRING}`);
 
-        done(); // Indicate the test has finished
+        complete(); // Indicate the test is complete.
     });
 
-    it("Colorization functions (background)", function(done) {
+    does("it correctly encode all background color directives?", function(complete) {
 
         when("it marks a string background as blackBg.").expect(Tinter.blackBg(DUMMY_STRING)).toEqual(`\x1b[1m\x1b[40m${DUMMY_STRING}\x1b[0m`, "it should be able mark a string with a black background.");
         when("it marks a string background as redBg.").expect(Tinter.redBg(DUMMY_STRING)).toEqual(`\x1b[1m\x1b[101m${DUMMY_STRING}\x1b[0m`, "it should be able mark a string with a red background.");
@@ -66,20 +66,20 @@ unit("Class: Tinter (Node/16-color [using CSS Named colors])", function() {
         when("it marks a string background as whiteBg.").expect(Tinter.whiteBg(DUMMY_STRING)).toEqual(`\x1b[1m\x1b[107m${DUMMY_STRING}\x1b[0m`, "it should be able mark a string with a white background.");
         when("it marks a string background as defaultBg.").expect(Tinter.defaultBg(DUMMY_STRING)).toEqual(`\x1b[49m${DUMMY_STRING}`, "it should be able mark a string with a default background..");
 
-        done(); // Indicate the test has finished
+        complete(); // Indicate the test is complete.
     });
 
-    it("Colorization functions (composite)", function(done) {
+    does("it correctly encode all composite color/style directives?", function(complete) {
 
         when("it marks a string with overlapping characteristics.").expect(Tinter.style(DUMMY_STRING, "yellow", "blue", "italic"), `\x1b[3m\x1b[1m\x1b[104m\x1b[1m\x1b[93m${DUMMY_STRING}\x1b[0m`);
         // expect(Tinter.rgb(DUMMY_STRING, [255,255,127], [192, 0, 55], "underline"), `\x1b[4m\x1b[1m\x1b[101m\x1b[1m\x1b[93m${DUMMY_STRING}\x1b[0m`, "it should degrade a truecolor to 16-color appropriately.");
         when("it uses ANSI named colors").expect(Tinter.Black(DUMMY_STRING)).toEqual(`\x1b[1m\x1b[30m${DUMMY_STRING}\x1b[0m`);
         when("it uses CSS4 named colors").expect(Tinter.rebeccapurple(DUMMY_STRING)).toEqual(`\x1b[1m\x1b[34m${DUMMY_STRING}\x1b[0m`);
 
-        done(); // Indicate the test has finished
+        complete(); // Indicate the test is complete.
     });
 
-    it("Truecolor functions", function(done) {
+    does("it handle all private RGB calculations accurately?", function(complete) {
 
         // private method
         when("it privately represents truecolor RGB values (All params)").expect(Tinter._styleTruecolor(DUMMY_STRING, [255,255,127], [192, 0, 55], "underline")).toEqual(`\x1b[4m\x1b[1m\x1b[48;2;192;0;55m\x1b[1m\x1b[38;2;255;255;127m${DUMMY_STRING}\x1b[0m`);
@@ -92,21 +92,21 @@ unit("Class: Tinter (Node/16-color [using CSS Named colors])", function() {
             .expect(Tinter._degrade(DUMMY_STRING, [200, 10, 21], [2, 0, 200], "italic"))
             .toEqual(`\x1b[3m\x1b[1m\x1b[104m\x1b[1m\x1b[91m${DUMMY_STRING}\x1b[0m`);
 
-        done(); // Indicate the test has finished
+        complete(); // Indicate the test is complete.
     });
 
-    it("Color degradation functions", function(done) {
+    does("it identify the correct degraded color name from RGB?", function(complete) {
 
-        when("it degrades a truecolor RGB value to black.").expect(Tinter._nearest16([10, 127, 0])).toEqual("black");
-        when("it degrades a truecolor RGB value to red.").expect(Tinter._nearest16([200, 10, 21])).toEqual("red");
-        when("it degrades a truecolor RGB value to green.").expect(Tinter._nearest16([0, 128, 0])).toEqual("green");
-        when("it degrades a truecolor RGB value to blue.").expect(Tinter._nearest16([2, 0, 200])).toEqual("blue");
-        when("it degrades a truecolor RGB value to yellow.").expect(Tinter._nearest16([200, 128, 0])).toEqual("yellow");
-        when("it degrades a truecolor RGB value to magenta.").expect(Tinter._nearest16([200, 10, 128])).toEqual("magenta");
-        when("it degrades a truecolor RGB value to cyan.").expect(Tinter._nearest16([0, 200, 128])).toEqual("cyan");
-        when("it degrades a truecolor RGB value to white.").expect(Tinter._nearest16([175, 200, 128])).toEqual("white");
+        when("it degrades a truecolor RGB value to the name black.").expect(Tinter._nearest16([10, 127, 0])).toEqual("black");
+        when("it degrades a truecolor RGB value to the name red.").expect(Tinter._nearest16([200, 10, 21])).toEqual("red");
+        when("it degrades a truecolor RGB value to the name green.").expect(Tinter._nearest16([0, 128, 0])).toEqual("green");
+        when("it degrades a truecolor RGB value to the name blue.").expect(Tinter._nearest16([2, 0, 200])).toEqual("blue");
+        when("it degrades a truecolor RGB value to the name yellow.").expect(Tinter._nearest16([200, 128, 0])).toEqual("yellow");
+        when("it degrades a truecolor RGB value to the name magenta.").expect(Tinter._nearest16([200, 10, 128])).toEqual("magenta");
+        when("it degrades a truecolor RGB value to the name cyan.").expect(Tinter._nearest16([0, 200, 128])).toEqual("cyan");
+        when("it degrades a truecolor RGB value to the name white.").expect(Tinter._nearest16([175, 200, 128])).toEqual("white");
 
-        done(); // Indicate the test has finished
+        complete(); // Indicate the test is complete.
     });
 
 });
